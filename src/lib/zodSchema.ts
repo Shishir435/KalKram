147ff93d34491cc7f8ec57a17abd1e34c6a7ebe6
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-06 11:42:46 +0530

    Refactor zodSchema.ts to use reusable schemas for email, password, and OTP

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -1,22 +1,29 @@
 import * as z from 'zod';
 
+export const PasswordSchema = z
+  .string()
+  .min(8, {
+    message: 'Password must be at least 8 characters long',
+  })
+  .max(56, {
+    message: 'Password must be less than 56 characters',
+  });
+export type PasswordSchemaType = z.infer<typeof PasswordSchema>;
+
+export const EmailSchema = z
+  .string()
+  .email({
+    message: 'Please enter a valid email',
+  })
+  .max(56, {
+    message: 'Email must be less than 56 characters',
+  });
+
+export type EmailSchemaTyper = z.infer<typeof EmailSchema>;
+
 export const LoginSchema = z.object({
-  email: z
-    .string()
-    .email({
-      message: 'Please enter a valid email',
-    })
-    .max(56, {
-      message: 'Email must be less than 56 characters',
-    }),
-  password: z
-    .string()
-    .min(8, {
-      message: 'Password must be at least 8 characters long',
-    })
-    .max(56, {
-      message: 'Password must be less than 56 characters',
-    }),
+  email: EmailSchema,
+  password: PasswordSchema,
 });
 
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
@@ -30,23 +37,37 @@ export const SignupSchema = z.object({
     .max(56, {
       message: 'Username must be less than 56 characters',
     }),
-  email: z
-    .string()
-    .email({
-      message: 'Please enter a valid email',
-    })
-    .max(56, {
-      message: 'Email must be less than 56 characters',
-    }),
-  password: z
-    .string()
-    .min(8, {
-      message: 'Password must be at least 8 characters long',
-    })
-    .max(56, {
-      message: 'Password must be less than 56 characters',
-    }),
+  email: EmailSchema,
+  password: PasswordSchema,
   policy: z.boolean().default(false),
 });
 
 export type SignupSchemaType = z.infer<typeof SignupSchema>;
+
+export const ForgetPassowrdSchema = z.object({
+  email: z.string().email({message: 'Please enter a valid email'}).max(56, {
+    message: 'Email must be less than 56 characters',
+  }),
+});
+
+export type ForgetPassowrdSchemaType = z.infer<typeof ForgetPassowrdSchema>;
+
+export const OtpSchema = z.object({
+  otp: z
+    .string()
+    .min(4, {
+      message: 'Otp must be 4 exactly characters long',
+    })
+    .max(4, {
+      message: 'Otp must be 4 exactly characters long',
+    }),
+});
+
+export type OtpType = z.infer<typeof OtpSchema>;
+
+export const ResetPasswordSchema = z.object({
+  newPassword: PasswordSchema,
+  confirmPassword: PasswordSchema,
+});
+
+export type ResetPasswordSchema = z.infer<typeof ResetPasswordSchema>;
