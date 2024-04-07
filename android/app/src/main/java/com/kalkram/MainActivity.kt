e0d1019b563601a478f1b01f8d1192e28ad26964
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-07 08:07:35 +0530

    addd navigation dependencies.

--- android/app/src/main/java/com/kalkram/MainActivity.kt
+++ android/app/src/main/java/com/kalkram/MainActivity.kt
@@ -4,7 +4,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
+// imp imports
+import android.os.Bundle;
 class MainActivity : ReactActivity() {
 
   /**
@@ -12,7 +13,12 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "KalKram"
-
+  /**
+  * Routing configuration
+  */ 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
