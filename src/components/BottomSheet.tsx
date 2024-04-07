5369f7ac56f13df51daa92a641ea296a0481f551
Shishir435 <shishirchaurasiya435@gmail.com>
2024-04-07 23:25:35 +0530

    Add BottomShee component in Home screen

---
+++ src/components/BottomSheet.tsx
@@ -0,0 +1,112 @@
+import React, {forwardRef, useImperativeHandle, useRef} from 'react';
+import {Image, Pressable, StyleSheet, View} from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import InterText from './InterText';
+import Button from './Button';
+import {HEADING, SUB_HEADING} from '../lib/color';
+import {ImageSourcePropType} from 'react-native';
+
+interface RBSheetRef {
+  open: () => void;
+  close: () => void;
+}
+type BottomSheetProps = {
+  imageSource: ImageSourcePropType;
+  sheetHeading: string;
+  sheetSubHEading: string;
+  sheetBtnTitle: string;
+  sheetBtnOnPress: () => void;
+};
+const BottomSheetComponent = forwardRef(
+  (
+    {
+      imageSource,
+      sheetHeading,
+      sheetBtnOnPress,
+      sheetBtnTitle,
+      sheetSubHEading,
+    }: BottomSheetProps,
+    ref,
+  ) => {
+    const bottomSheetRef = useRef<RBSheetRef>(null);
+    useImperativeHandle(ref, () => ({
+      open: () => {
+        bottomSheetRef.current?.open();
+      },
+      close: () => {
+        bottomSheetRef.current?.close();
+      },
+    }));
+
+    return (
+      <RBSheet
+        ref={bottomSheetRef}
+        closeOnPressMask={true}
+        height={500}
+        customStyles={{
+          container: {
+            gap: 32,
+            alignItems: 'center',
+            borderTopLeftRadius: 24,
+            borderTopRightRadius: 24,
+            paddingHorizontal: 24,
+            paddingBottom: 24,
+            paddingTop: 10,
+          },
+        }}>
+        <>
+          <Pressable
+            style={styles.sheetCloseButton}
+            onPress={() => {
+              bottomSheetRef.current?.close();
+            }}
+          />
+          <Image source={imageSource} style={styles.sheetImage} />
+          <View style={styles.sheetTextContainer}>
+            <InterText style={styles.sheetHeading}>{sheetHeading}</InterText>
+            <InterText style={styles.sheetSubHeading}>
+              {sheetSubHEading}
+            </InterText>
+          </View>
+          <Button
+            title={sheetBtnTitle}
+            onPress={sheetBtnOnPress}
+            buttonStyle={styles.actionButton}
+          />
+        </>
+      </RBSheet>
+    );
+  },
+);
+
+const styles = StyleSheet.create({
+  sheetCloseButton: {
+    width: 60,
+    height: 4,
+    backgroundColor: '#00000033',
+    borderRadius: 100,
+  },
+  sheetImage: {
+    width: 202,
+    height: 168,
+  },
+  sheetTextContainer: {
+    gap: 12,
+  },
+  sheetHeading: {
+    color: HEADING,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  sheetSubHeading: {
+    color: SUB_HEADING,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  actionButton: {
+    width: '95%',
+  },
+});
+
+export default BottomSheetComponent;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,23 +1,49 @@
-import {ImageBackground, StyleSheet, Text, View} from 'react-native';
-import React, {useContext, useEffect, useState} from 'react';
-import {AppwriteContext} from '../../appwrite/appwriteContext';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {RoutesParamList} from '../../types';
-import Button from '../../components/Button';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {ImageBackground, StyleSheet, View} from 'react-native';
 import Snackbar from 'react-native-snackbar';
+import {AppwriteContext} from '../../appwrite/appwriteContext';
+import Button from '../../components/Button';
 import ErrorComponent from '../../components/Errro';
 import InterText from '../../components/InterText';
+import {RoutesParamList} from '../../types';
+import BottomSheetComponent from '../../components/BottomSheet';
+
 type HomeScreenProps = NativeStackScreenProps<
   RoutesParamList,
   keyof RoutesParamList
 >;
+interface RBSheetRef {
+  open: () => void;
+  close: () => void;
+}
 const Home = ({navigation}: HomeScreenProps) => {
+  const refRBSheet = useRef<RBSheetRef | null>(null);
   const {appwrite, isLoggedIn, setIsLoggedIn} = useContext(AppwriteContext);
+  const [showBottomSheet, setShowBottomSheet] = useState(true);
   const [error, setError] = useState('');
   const [user, setUser] = useState({
     username: '',
     email: '',
   });
+  const handleLogout = () => {
+    appwrite
+      .logout()
+      .then(() => {
+        setIsLoggedIn(false);
+        Snackbar.show({
+          text: 'Logout successfull',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+        navigation.navigate('Login');
+      })
+      .catch((err: any) => {
+        Snackbar.show({
+          text: err.message,
+          duration: Snackbar.LENGTH_LONG,
+        });
+      });
+  };
   useEffect(() => {
     if (isLoggedIn) {
       appwrite
@@ -29,6 +55,11 @@ const Home = ({navigation}: HomeScreenProps) => {
               email: resp.email,
             };
             setUser(userObj);
+            // open bottom sheet
+            if (showBottomSheet) {
+              refRBSheet.current?.open();
+              setShowBottomSheet(false);
+            }
           }
         })
         .catch((err: any) => {
@@ -37,36 +68,29 @@ const Home = ({navigation}: HomeScreenProps) => {
     } else {
       navigation.navigate('Login');
     }
-  }, [appwrite, isLoggedIn, navigation]);
+  }, [appwrite, isLoggedIn, navigation, showBottomSheet]);
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require('../../../assets/images/homebg.png')}
         style={styles.imageBackground}>
-        <Text>Home</Text>
         {error && <ErrorComponent error={error} />}
         {user && <InterText>{user.username}</InterText>}
         <Button
           title="Logout"
-          onPress={() => {
-            appwrite
-              .logout()
-              .then(() => {
-                setIsLoggedIn(false);
-                Snackbar.show({
-                  text: 'Logout successfull',
-                  duration: Snackbar.LENGTH_SHORT,
-                });
-                // navigation.navigate('Login');
-              })
-              .catch((err: any) => {
-                Snackbar.show({
-                  text: err.message,
-                  duration: Snackbar.LENGTH_LONG,
-                });
-              });
-          }}
+          buttonStyle={styles.button}
+          onPress={handleLogout}
         />
+        <View>
+          <BottomSheetComponent
+            imageSource={require('../../../assets/images/Illustration-Success.png')}
+            sheetHeading="Login Successful"
+            sheetSubHEading="An event has been created and the invite has been sent to you on mail."
+            ref={refRBSheet}
+            sheetBtnTitle="LogOut"
+            sheetBtnOnPress={handleLogout}
+          />
+        </View>
       </ImageBackground>
     </View>
   );
@@ -77,12 +101,13 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+  },
+  button: {
+    width: 100,
+    borderRadius: 0,
   },
   imageBackground: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     resizeMode: 'cover',
   },
